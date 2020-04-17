8227c675d391689dfb4172e446e1ced89974da31
Laura Gemmell <laura-gemmell@hotmail.com>
2020-04-17 18:30:34 +0100

    optimising code (removed forEach loop, changed IF..ELSE to switches where possible, declared all variables)

--- js/test.js
+++ js/test.js
@@ -1,25 +1,25 @@
 // SETUP //
 // Get all the values needed for the simulation (these are the default varaibles from the inputs)
-n = document.getElementById("boids").value; // number of boids
-r = document.getElementById("radius").value;  // radius of neighbourhood
-v = document.getElementById("velocity").value; // velocity of boids
+var n = document.getElementById("boids").value; // number of boids
+// var r = document.getElementById("radius").value;  // radius of neighbourhood
+var v = document.getElementById("velocity").value; // velocity of boids
 
-weight_ali = document.getElementById("alignment").value ; // weight of alignment
-weight_coh = document.getElementById("cohesion").value; // weight of cohesion
-weight_sep = document.getElementById("separation").value; // weight of separation
+// var weight_ali = document.getElementById("alignment").value ; // weight of alignment
+// var weight_coh = document.getElementById("cohesion").value; // weight of cohesion
+// var weight_sep = document.getElementById("separation").value; // weight of separation
 
 // variables that don't change
-w = 800; // width of area
-h = 800; // height of area
-sep_cof = 100; // separation coefficient
+var w = 800; // width of area
+var h = 800; // height of area
+var sep_cof = 100; // separation coefficient
 
 // display variables on website
-document.getElementById("boidsOutput").innerHTML = n; // number of boids
-document.getElementById("radiusOutput").innerHTML = r;  // radius of neighbour
-document.getElementById("aliOutput").innerHTML = Math.round(weight_ali*100) + "%" ; // weight of alignment as percentage
-document.getElementById("cohOutput").innerHTML = Math.round(weight_coh*100) + "%" ; // weight of cohesion as percentage
-document.getElementById("sepOutput").innerHTML = Math.round(weight_sep*100) + "%" ; // weight of separation as percentage
-document.getElementById("velOutput").innerHTML = v; // velocity of boids
+// document.getElementById("boidsOutput").innerHTML = n; // number of boids
+// document.getElementById("radiusOutput").innerHTML = r;  // radius of neighbour
+// document.getElementById("aliOutput").innerHTML = Math.round(weight_ali*100) + "%" ; // weight of alignment as percentage
+// document.getElementById("cohOutput").innerHTML = Math.round(weight_coh*100) + "%" ; // weight of cohesion as percentage
+// document.getElementById("sepOutput").innerHTML = Math.round(weight_sep*100) + "%" ; // weight of separation as percentage
+// document.getElementById("velOutput").innerHTML = v; // velocity of boids
 
 // Class for creating Boids, each boid has an x and y starting value that is a random number between 0 and 100
 function Boid(){
@@ -37,7 +37,7 @@ for (i = 0 ; i < n ; i++){
 }
 
 // Make a variable for x and y for plotting
-function plotBoids(n){
+function plotBoids(n,boids){
     var x_plot = [];
     var y_plot = [];
     for (i = 0 ; i < n ; i++){
@@ -51,12 +51,13 @@ function plotBoids(n){
 // Work out the neighbourhood of each boid 
 // I.e. is another boid within a certain distance (r) from that boid
 // Uses Pythagoras theorem
-function neighbourhood(agent,boids,n){
-    neighbours = []
-    r = document.getElementById("radius").value; 
+function neighbourhood(n,agent,boids){
+    var neighbours = []
+    var r = document.getElementById("radius").value; 
     document.getElementById("radiusOutput").innerHTML = r; 
     for (i = 0 ; i < n ; i++){
-        if (i != agent && Math.sqrt((boids[agent].x - boids[i].x)^2 + (boids[agent].y - boids[i].y)^2) < r){
+        // if (i != agent && Math.sqrt((boids[agent].x - boids[i].x)^2 + (boids[agent].y - boids[i].y)^2) < r){
+        if (i != agent && Math.sqrt((Math.pow(boids[agent].x - boids[i].x,2) + Math.pow(boids[agent].y - boids[i].y,2))) < r){
             neighbours.push(boids[i]);
         }
     }
@@ -67,9 +68,9 @@ function neighbourhood(agent,boids,n){
 // Find the average position of all neighbours
 // Use this to compute angle necessary to steer towards this centre
 function cohesion(agent,neighbours){
-    x_coh = 0;
-    y_coh = 0;
-    num = neighbours.length; // number of neighbours
+    var x_coh = 0;
+    var y_coh = 0;
+    var num = neighbours.length; // number of neighbours
     for (i = 0 ; i < num ; i++){
         x_coh += neighbours[i].x;
         y_coh += neighbours[i].y;
@@ -77,8 +78,8 @@ function cohesion(agent,neighbours){
     x_coh = x_coh / num;
     y_coh = y_coh / num;
 
-    mag_coh = Math.sqrt(x_coh^2 + y_coh^2);
-    ang_coh = Math.atan2(y_coh, x_coh);
+    var mag_coh = Math.sqrt(x_coh^2 + y_coh^2);
+    var ang_coh = Math.atan2(y_coh, x_coh);
     if (ang_coh == NaN){
         ang_coh = 0;
     }
@@ -89,10 +90,10 @@ function cohesion(agent,neighbours){
 // 
 function separation(agent,neighbours,boids,sep_cof){
 
-    x_sep = 0;
-    y_sep = 0;
-    count = 0;
-    num = neighbours.length; // number of neighbours
+    var x_sep = 0;
+    var y_sep = 0;
+    var count = 0;
+    var num = neighbours.length; // number of neighbours
     for (i = 0 ; i < num ; i++){
         if (Math.sqrt((boids[agent].x - neighbours[i].x)^2 + (y_sep += boids[agent].y - neighbours[i].y)^2) < sep_cof){
             x_sep += neighbours[i].x;
@@ -105,31 +106,33 @@ function separation(agent,neighbours,boids,sep_cof){
         y_sep = y_sep / count;
     }
     
-    mag_sep = x_sep^2 + y_sep^2;
-    ang_sep = Math.atan2(y_sep, x_sep);
+    var mag_sep = x_sep^2 + y_sep^2;
+    var ang_sep = Math.atan2(y_sep, x_sep);
     return [mag_sep, ang_sep];
 }
 
 function alignment(agent,neighbours,boids){
-    angle = 0;
-    num = neighbours.length; // number of neighbours
+    var angle = 0;
+    var num = neighbours.length; // number of neighbours
     for (i = 0 ; i < num ; i++){
         angle += neighbours[i].theta;
     }
-    ang_ali = angle / num;
+    var ang_ali = angle / num;
     return ang_ali;
 }
 
 function updateBoid(agent,boids){
-    neighbours = neighbourhood(agent,boids,n);
+    var neighbours = neighbourhood(n,agent,boids);
+    
     
     if (neighbours.length > 0){
+        var ang_coh, ang_sep, ang_ali,new_x, new_y = 0;
+        // var new_x, new_y;
+        // var mag_coh, ang_coh, mag_sep, ang_sep, ali_ang = 0;
 
-        var mag_coh, ang_coh, mag_sep, ang_sep, ali_ang = 0;
-
-        mag_coh = cohesion(agent,neighbours)[0];
+       // var mag_coh = cohesion(agent,neighbours)[0];
         ang_coh = cohesion(agent,neighbours)[1];
-        mag_sep = separation(agent,neighbours,boids,sep_cof)[0];
+       // var mag_sep = separation(agent,neighbours,boids,sep_cof)[0];
         ang_sep = separation(agent,neighbours,boids,sep_cof)[1];
         ang_ali = alignment(agent,neighbours,boids);
 
@@ -155,54 +158,123 @@ function updateBoid(agent,boids){
         new_y = boids[agent].y - v * (Math.round(Math.random()) * 2 - 1) * Math.random(); 
     }
 
-    if (new_x > w){
-        new_x = (new_x - w) - w;
+    // if (new_x > w){
+    //     new_x = (new_x - w) - w;
+    // }
+
+    // if (new_y > h){
+    //     new_y = (new_y - h) - h;
+    // }
+
+    // if (new_x < - w){
+    //     new_x = (new_x + w) + w;
+    // }
+
+    // if (new_y < - h){
+    //     new_y = (new_y + h) + h;
+    // }
+
+
+    switch(true){
+        case new_x > w:
+            boids[agent].x = (new_x - w) - w;
+            break;
+        case new_x < -w:
+            boids[agent].x = (new_x + w) + w;
+            break;
+        default:
+            boids[agent].x = new_x;
+            break;
+
     }
 
-    if (new_y > h){
-        new_y = (new_y - h) - h;
+    switch(true){
+        case new_y > h:
+            boids[agent].y = (new_y - h) - h;
+            break;
+        case new_y < -h:
+            boids[agent].h = (new_y + h) + h;
+            break;
+        default:
+            boids[agent].y = new_y;
+            break;
+
     }
 
-    if (new_x < - w){
-        new_x = (new_x + w) + w;
-    }
-
-    if (new_y < - h){
-        new_y = (new_y + h) + h;
-    }
-
-    boids[agent].x = new_x;
-    boids[agent].y = new_y;
-    return;
+    // boids[agent].x = new_x;
+    // boids[agent].y = new_y;
+    return boids;
 }
 
 
 
-function updateSim(n,boids,h,w){
+function updateSim(n,boids){
+    // var n = document.getElementById("boidsOutput").innerHTML;
     var new_n = document.getElementById("boids").value;
-    if (new_n != n){
-        document.getElementById("updateNeeded").innerHTML = "Please stop and restart to update number of boids"; 
+    switch(new_n){
+        case n:
+            document.getElementById("updateNeeded").innerHTML = ""; 
+            break;
+        default:
+            document.getElementById("updateNeeded").innerHTML = "Please stop and restart to update number of boids"; 
+            break;
     }
-    else {
-        document.getElementById("updateNeeded").innerHTML = ""; 
+    
+    // if (new_n === n) {
+    //     var count = 0;
+    //     for (var boid in boids){
+    //         updateBoid(count,boids);
+    //         count++;
+    //     }
+    //     plotBoids(n,boids);
+    //     updatePlot(boids,n,h,w);
+        
+    // }
+    // else {
+    //     n = document.getElementById("boids").value; // number of boids
+    //     document.getElementById("boidsOutput").innerHTML = n; 
+    //     boids = []; // array of all the n boids in the simulation
+
+    //     // Add a Boid object for each boid in the array
+    //     for (i = 0 ; i < n ; i++){
+    //         boids[i] = new Boid();
+    //     }
+    //     plotBoids(n,boids);
+    //     updatePlot(boids,n,h,w)
+    // }
+
+    // if (new_n != n){
+    //     document.getElementById("updateNeeded").innerHTML = "Please stop and restart to update number of boids"; 
+    // }
+    // else {
+    //     document.getElementById("updateNeeded").innerHTML = ""; 
+    // }
+
+    var count = 0;
+    for (var boid in boids){
+        updateBoid(count,boids);
+        count++;
     }
+    
+    // boids.forEach(boid => {
+    // //    agent = boids.indexOf(boid);
+    //     console.log(boids.indexOf(boid));
+    // //    updateBoid(agent,boids);
+    // });
 
-    boids.forEach(boid => {
-       agent = boids.indexOf(boid);
-       updateBoid(agent,boids);
-    });
-
-    plotBoids(n);
+    plotBoids(n,boids);
     updatePlot(boids,n,h,w);
-    return;
+    return boids;
 }
 
 function updatePlot(boids,n,h,w){
     Plotly.react( graph, [{
-        x: plotBoids(n)[0],
-        y: plotBoids(n)[1],
+        x: plotBoids(n,boids)[0],
+        y: plotBoids(n,boids)[1],
         mode: 'markers',
-        type: 'scatter' }], {
+        type: 'scatter' 
+    }], 
+    {
         margin: {
             l: 0,
             r: 0,
@@ -233,10 +305,10 @@ function updatePlot(boids,n,h,w){
 }
 
 
-graph = document.getElementById('flockingSim');
+const graph = document.getElementById('flockingSim');
 Plotly.plot( graph, [{
-    x: plotBoids(n)[0],
-    y: plotBoids(n)[1],
+    x: plotBoids(n,boids)[0],
+    y: plotBoids(n,boids)[1],
     mode: 'markers',
     type: 'scatter' }], {
     margin: {
@@ -269,7 +341,7 @@ Plotly.plot( graph, [{
 
 var updating = window.setInterval(function(){
     updateSim(n,boids)
-  }, 2000);
+  }, 5);
 
 var startButton = document.getElementById("start");
 var stopButton = document.getElementById("stop");
@@ -279,17 +351,17 @@ stopButton.disabled=false;
 restartButton.disabled=true;
 
 var startSim = function(){
-    console.log("Start");
+    // console.log("Start");
     updating = window.setInterval(function(){
         updateSim(n,boids)
-      }, 2000);
+      }, 1);
     startButton.disabled=true;
     stopButton.disabled=false;
     restartButton.disabled=true;
 }
 
 var stopSim = function(){
-    console.log("Stop");
+    // console.log("Stop");
     window.clearInterval(updating) // clear the timer and so stop the clock
     stopButton.disabled=true;
     startButton.disabled=false;
@@ -297,7 +369,7 @@ var stopSim = function(){
 }
 
 var restartSim = function(){
-    console.log("Restart");
+    // console.log("Restart");
     n = document.getElementById("boids").value; // number of boids
     document.getElementById("boidsOutput").innerHTML = n; 
     document.getElementById("updateNeeded").innerHTML = ""; 
@@ -315,4 +387,3 @@ var restartSim = function(){
 
 // is separation coefficient correct?
 // should all algorithms be calculated just using neighbours?
-// 
